3cb684745bc111832f68d37ec7645ddc2b172984
AXEL NETWORK <35977154+axelnetwork@users.noreply.github.com>
2021-11-08 20:48:33 +0200

    v3.1.0

--- axel-install.sh
+++ axel-install.sh
@@ -16,7 +16,6 @@ COIN_NAME="AXEL"
 COIN_PORT=32323
 COIN_VERSION="3.1.0"
 COIN_REPO="https://github.com/axelnetwork/${COIN_NAME}/releases/download/v${COIN_VERSION}/${COIN_NAME}-${COIN_VERSION}-linux-gnu.tar.gz"
-
 COIN_NAME_LOWER=$(echo $COIN_NAME | awk '{ print tolower($0) }')
 CONFIG_FILE="${COIN_NAME_LOWER}.conf"
 CONFIGFOLDER="/root/.${COIN_NAME_LOWER}"
