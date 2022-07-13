c57ef9dfd51ae8b6268b3a2812a3ee6f6ab17d1f
AXEL NETWORK <35977154+axelnetwork@users.noreply.github.com>
2022-07-13 11:47:00 +0300

    'add-apt-repository command not found' fix

--- axel-install.sh
+++ axel-install.sh
@@ -255,7 +255,8 @@ function prepare_system() {
   echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
   sudo add-apt-repository -y ppa:bitcoin/bitcoin
   apt-get update # >/dev/null 2>&1
-  apt-get install -y wget curl ufw binutils net-tools mc libdb4.8-dev libdb4.8++-dev libboost-all-dev libzmq3-dev libminiupnpc-dev # >/dev/null 2>&1
+  apt-get install -y wget curl ufw binutils net-tools mc libdb4.8-dev libdb4.8++-dev libboost-all-dev libzmq3-dev libminiupnpc-dev software-properties-common # >/dev/null 2>&1
+  apt-get update # >/dev/null 2>&1
 }
 
 
